341b3add9cd5038b2c2652b9bae5efc851300177
muris2016 <rsemuris2016@gmail.com>
2016-01-05 21:07:17 +0100

    INVITE implementado

--- uaclient.py
+++ uaclient.py
@@ -6,6 +6,8 @@ import socket
 import hashlib
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import time
+
 
 class ConfigUAHandler(ContentHandler):
     def __init__ (self):
@@ -22,6 +24,15 @@ class ConfigUAHandler(ContentHandler):
         if name in self.labels:
             my_dict = {key: attrs.get(key, '') for key in self.labels[name]}
             self.conf_ua_dict[name] =  my_dict
+
+def write_log(log_path, event, ip = '', port = '', msg = ''):
+    msg = ' '.join(msg.split())
+    with open(log_path, 'a') as outfile:
+        hour = str(time.strftime('%Y%m%d%H%M%S', time.gmtime()))
+        if ip != '':
+            outfile.write('%s %s %s:%s: %s\n' % (hour, event, ip, str(port), msg))
+        else:
+            outfile.write('%s %s\n' % (hour, event))
 
 def get_tags(config, Handler):
     parser = make_parser()
@@ -43,40 +54,58 @@ def get_hash(nonce, passwd):
     return m.hexdigest()
 
 
-def connect_to_pr(msg, my_socket, conf_ua_dict):
-    server = conf_ua_dict['regproxy']['ip']
+def connect_to_proxy(msg, my_socket, conf_ua_dict):
+    ip_server = conf_ua_dict['regproxy']['ip']
     port = int(conf_ua_dict['regproxy']['puerto'])
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((server, port))
+    my_socket.connect((ip_server, port))
     my_socket.send(bytes(msg, 'utf-8'))
 
-def register(conf_ua_dict, expires):
+    log_path = conf_ua_dict['log']['path']
+    write_log(log_path, 'Sent to', ip_server, port, msg)
+
+def register(my_socket, conf_ua_dict, expires):
     expires = int(expires)
     username = conf_ua_dict['account']['username']
     msg = 'REGISTER sip:%s SIP/2.0\r\nExpires: %s\r\n\r\n' % (username, expires)
     print(msg)
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    connect_to_pr(msg, my_socket, conf_ua_dict)
+    connect_to_proxy(msg, my_socket, conf_ua_dict)
 
-    data = my_socket.recv(1024).decode('utf-8').split()
-    print(" ".join(data))
-    if 'Unauthorized' in data:
-        nonce = data[-1].split('=')[-1]
+    data = my_socket.recv(1024).decode('utf-8')
+    print(data)
+    if 'Unauthorized' in data.split():
+        nonce = data.split()[-1].split('=')[-1]
         passwd = conf_ua_dict['account']['passwd']
         response = get_hash(nonce, passwd)
         msg = 'REGISTER sip:%s SIP/2.0\r\nExpires: %s\r\n' % (username, expires)
         msg += 'Authorization: response=%s\r\n\r\n' % (response)
         print(msg)
-        connect_to_pr(msg, my_socket, conf_ua_dict)
+        connect_to_proxy(msg, my_socket, conf_ua_dict)
+        data = my_socket.recv(1024).decode('utf-8')
+        print(data)
+
+def invite(my_socket, conf_ua_dict, login):
+    username = conf_ua_dict['account']['username']
+    rtpaudio_port = conf_ua_dict['rtpaudio']['puerto']
+    msg = 'INVITE sip:%s SIP/2.0\r\n' % (login)
+    msg += 'Content-Type: application/sdp\r\n\r\n'
+    msg += 'v=0\r\no=%s 127.0.0.1\r\ns=mp3p2p\r\nt=0\r\n' % (username)
+    msg += 'm=audio %s RTP\r\n\r\n' % (rtpaudio_port)
+    connect_to_proxy(msg, my_socket, conf_ua_dict)
 
 def bye():
     print('bye')
 
 if __name__ == "__main__":
-    methods = {"REGISTER": register, "BYE": bye}
+    methods = {"REGISTER": register, "INVITE": invite, "BYE": bye}
     config, method, option = take_args()
     conf_ua_dict = get_tags(config, ConfigUAHandler)
+    log_path = conf_ua_dict['log']['path']
+
+
+    write_log(log_path, 'Starting...')
     # try:
-    methods[method](conf_ua_dict, option)
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    methods[method](my_socket, conf_ua_dict, option)
     # except:
     #     sys.exit("Usage: python uaclient.py config method option")
