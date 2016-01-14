a2574b7ad622b4480ee3da139ac8f2515158cec6
muris2016 <rsemuris2016@gmail.com>
2016-01-14 10:35:02 +0100

    Mejora en general del codigo y pep8

--- uaclient.py
+++ uaclient.py
@@ -5,6 +5,7 @@ import os
 import socket
 import hashlib
 import threading
+import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from uaserver import ConfigHandler
@@ -41,7 +42,7 @@ def send_rtp(rtp_port, audio_file):
     print('Sending audio has finished')
 
 
-def connect_to_proxy(msg, my_socket, config_dict, ack=False):
+def connect_to_proxy(msg, my_socket, ack=False):
     ip_server = config_dict['regproxy']['ip']
     port = int(config_dict['regproxy']['puerto'])
 
@@ -59,35 +60,35 @@ def connect_to_proxy(msg, my_socket, config_dict, ack=False):
         return data
 
 
-def register(my_socket, config_dict, expires):
+def register(my_socket, expires):
     write_log(config_dict, 'Starting...')
-    expires = int(expires)
     username = config_dict['account']['username']
-    port_uas = int(config_dict['uaserver']['puerto'])
+    port_uas = config_dict['uaserver']['puerto']
     msg = 'REGISTER sip:%s:%s SIP/2.0\r\n' % (username, port_uas)
     msg += 'Expires: %s\r\n\r\n' % (expires)
-    data = connect_to_proxy(msg, my_socket, config_dict)
+    data = connect_to_proxy(msg, my_socket)
 
     if 'Unauthorized' in data.split():
         nonce = data.split()[-1].split('=')[-1].strip('"')
         passwd = config_dict['account']['passwd']
         response = get_hash(nonce, passwd)
-        msg += 'Authorization: Digest response="%s"\r\n\r\n' % (response)
-        connect_to_proxy(msg, my_socket, config_dict)
+        resp = '\r\nAuthorization: Digest response="%s"\r\n' % (response)
+        msg = msg.replace('\r\n', resp, 1)
+        connect_to_proxy(msg, my_socket)
 
 
-def invite(my_socket, config_dict, login):
+def invite(my_socket, login):
     username = config_dict['account']['username']
     rtpaudio_port = config_dict['rtpaudio']['puerto']
     msg = 'INVITE sip:%s SIP/2.0\r\n' % (login)
     msg += 'Content-Type: application/sdp\r\n\r\n'
     msg += 'v=0\r\no=%s 127.0.0.1\r\ns=mp3p2p\r\nt=0\r\n' % (username)
     msg += 'm=audio %s RTP\r\n\r\n' % (rtpaudio_port)
-    data = connect_to_proxy(msg, my_socket, config_dict)
+    data = connect_to_proxy(msg, my_socket)
 
     if '100 Trying' in data and '180 Ring' and data and '200 OK' in data:
         msg = 'ACK sip:%s SIP/2.0\r\n\r\n' % (login)
-        connect_to_proxy(msg, my_socket, config_dict, ack=True)
+        connect_to_proxy(msg, my_socket, ack=True)
 
         rtpaudio_port = config_dict['rtpaudio']['puerto']
         rtp_port = data.split('m=')[1].split()[1]
@@ -96,32 +97,38 @@ def invite(my_socket, config_dict, login):
         t1 = threading.Thread(target=run_cvlc, args=(rtpaudio_port,))
         t2 = threading.Thread(target=send_rtp, args=(rtp_port, audio_file,))
         t1.start()
+        time.sleep(0.2)
         t2.start()
 
 
-def bye(my_socket, config_dict, login):
+def bye(my_socket, login):
     msg = 'BYE sip:%s SIP/2.0\r\n\r\n' % (login)
-    data = connect_to_proxy(msg, my_socket, config_dict)
+    data = connect_to_proxy(msg, my_socket)
     if '200 OK' in data:
         os.system('killall mp32rtp 2> /dev/null')
         write_log(config_dict, 'Finishing...\n\r')
 
 
-if __name__ == '__main__':
-    methods = {'REGISTER': register, 'INVITE': invite, 'BYE': bye}
+def main():
     global response_codes
     response_codes = ['Bad Request', 'User Not Found', 'Method Not Allowed']
     response_codes += ['Address Incomplete', 'Unauthorized', 'OK', 'Decline']
-    response_codes += ['Temporarily Unavailable', 'Forbidden']
+    response_codes += ['Temporarily Unavailable', 'Forbidden', 'Conflict']
     config, method, option = take_args()
+    global config_dict
     config_dict = get_tags(config, ConfigHandler)
 
+    return method, option
+
+if __name__ == '__main__':
+    methods = {'REGISTER': register, 'INVITE': invite, 'BYE': bye}
+    method, option = main()
     try:
         my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        methods[method](my_socket, config_dict, option)
+        methods[method](my_socket, option)
 
     except KeyError:
-        sys.exit('Usage: python uaclient.py config method option')
+        print('Usage: python uaclient.py config method option')
 
     except ConnectionRefusedError:
         ip_server = config_dict['regproxy']['ip']
